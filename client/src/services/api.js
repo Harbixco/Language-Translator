342da6f90a3915e26ab2eba7c006b68fe20e6e25
hagios <agbajeabiodunkay@gmail.com>
2025-07-21 18:12:31 +0100

    upgrade the vite

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Translate = async (params) => {
-  return await axios.post("http://localhost:4000/translate", params);
+  return await axios.post("https://language-translator-api.vercel.app/translate", params);
   // return await axios.post("/api/translate", params);
 };
