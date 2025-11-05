70eb3f025e05e01b7c7810a8d45f3e7203c7fa17
hagios <agbajeabiodunkay@gmail.com>
2025-11-05 10:40:01 +0100

    update the resume and made chnages in other parts

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,4 +1,4 @@
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logo.jpg";
 import StudentPhoto from "../assets/avater.jpg";
 import aboutMeModal from "./core/ModalHandler";
 import AboutMe from "./AboutMe";
